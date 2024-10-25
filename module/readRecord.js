95ce0f7ffe751aa1eb3dfa746dd124c183d6f40c
Janesummer <1752321720@qq.com>
2024-10-25 14:03:54 +0800

    ✨ feat:更新

--- module/readRecord.js
+++ module/readRecord.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 
 let read = (req, resp) => {
   let filename = req.params.name;
+  if (filename.indexOf('favicon') !== -1) {
+    resp.end();
+    return
+  }
   let data = fs.readFileSync(path.join(__dirname, `../static/${filename}.txt`), {
     encoding: 'utf8'
   });
